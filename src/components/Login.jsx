7dde56be623b9b5e7be5dd9e8cb5789d74fede7f
Michael Dinh <mdinh253@cs.washington.edu>
2026-02-05 00:46:37 -0800

    basic login and registration

---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import "../App.css"
+
+function Login() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        const userData = JSON.parse(localStorage.getItem(data.email));
+        if (userData) {
+            if (userData.password === data.password) {
+                console.log(userData.name + "You are successfully logged in!");
+            } else {
+                console.log("Email or Password is not matching with our record");
+            }
+        } else {
+            console.log("Email or Password is not matching with our record");
+        }
+    };
+
+    return (
+        <>
+            <h2>Login Form</h2>
+
+            <form className="App" onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    type="email"
+                    {...register("email", { required: true })}
+                    placeholder="Email"
+                />
+                {errors.email && <span style={{ color: "red" }}>*Email* is mandatory</span>}
+
+                <input
+                    type="password"
+                    {...register("password", { required: true })}
+                    placeholder="Password"
+                />
+                {errors.password && <span style={{ color: "red" }}>*Password* is mandatory</span>}
+
+                <input type="submit" style={{ backgroundColor: "#a1eafb" }} />
+            </form>
+            <p>
+                <Link to="/register">Register</Link>
+            </p>
+        </>
+    );
+}
+
+export default Login;
